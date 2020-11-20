0a934dd6643d05cafa23d06e728383ea7c6ea1ea
AlfonsoHeme <alfonso.heme@gmail.com>
2020-11-19 19:20:49 -0600

    Avace Siete

--- miPrimerMVCC/Controllers/PersonasController.cs
+++ miPrimerMVCC/Controllers/PersonasController.cs
@@ -14,6 +14,7 @@ using Web100.Models.ViewModel;
 using System.Web.Configuration;
 using System.Data;
 using System.Collections.Specialized;
+using Newtonsoft.Json;
 
 namespace Web100.Controllers
 {
@@ -30,7 +31,6 @@ namespace Web100.Controllers
 
         public JsonResult ListaPersonas()
         {
-            /*
             List<ListaPersona> lista = new List<ListaPersona>();
 
             lista = (from R in db.Persona
@@ -41,9 +41,222 @@ namespace Web100.Controllers
                          ApellidoPaterno = R.ApellidoPaterno,
                          ApellidoMaterno = R.ApellidoMaterno,
                          Estatus = (bool)R.Estatus
-                     }).ToList();*/
+                     }).ToList();
 
-            return Json(cPersona.ListaPersonas(), JsonRequestBehavior.AllowGet);
+            //return Json(cPersona.ListaPersonas(), JsonRequestBehavior.AllowGet);
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ObtienePersona(int Id)
+        {
+            try
+            {
+                List<ListaPersona> lista = new List<ListaPersona>();
+
+                List<string> ListaPersona = new List<string>();
+
+                dynamic oPerson = new ListaPersona();
+
+                using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spR_ObtinePersona", connection);
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader Read = command.ExecuteReader();
+
+                    while (Read.Read())
+                    {
+                        lista.Add(new ListaPersona
+                        {
+                            Id = Convert.ToInt32(Read["Id"]),
+                            Nombre = Read["Nombre"].ToString(),
+                            ApellidoPaterno = Read["ApellidoPaterno"].ToString(),
+                            ApellidoMaterno = Read["ApellidoMaterno"].ToString(),
+                            Estatus = Convert.ToBoolean(Read["Estatus"]),
+                        });
+                    }
+                }
+
+                var lts = lista.Find(x => x.Id.Equals(Id));
+
+                return Json(lts, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Json(ex.Message.ToString(), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult AgregarPersona(string Nombre, string ApellidoPaterno, string ApellidoMaterno, Boolean Estatus)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spC_CreaPersona", connection);
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@ApellidoPaterno", ApellidoPaterno);
+                    command.Parameters.AddWithValue("@ApellidoMaterno", ApellidoMaterno);
+                    command.Parameters.AddWithValue("@Estatus", Estatus);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    int iResultado = command.ExecuteNonQuery();
+
+                    return Json(new { Resultado = iResultado }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Json(ex.Message.ToString(), JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        public ActionResult AgregarPersona1(string Nombre, string ApellidoPaterno, string ApellidoMaterno, Boolean Estatus)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spC_CreaPersona", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@ApellidoPaterno", ApellidoPaterno);
+                    command.Parameters.AddWithValue("@ApellidoMaterno", ApellidoMaterno);
+                    command.Parameters.AddWithValue("@Estatus", Estatus);
+
+                    int iResultado = command.ExecuteNonQuery();
+
+                    return Content("" + iResultado);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Content(ex.Message.ToString());
+                }
+            }
+        }
+
+        public ActionResult EliminarPersona(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                DataTable dts = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spD_EliminaPersona", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", Id);
+                   
+                    int i = command.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dts);
+
+                    string dato = dts.Rows[0]["Id"].ToString();
+                    return Content("" + i);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Content(ex.Message.ToString());
+                }
+            }
+        }
+
+        public int EliminarPersona2(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                DataTable dts = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spD_EliminaPersona", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", Id);
+
+                    int i = command.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dts);
+
+                    string dato = dts.Rows[0]["Id"].ToString();
+
+                    return i;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return 0;
+                }
+            }
+        }
+
+        public JsonResult EliminarPersona3(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                DataTable dts = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spD_EliminaPersona", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", Id);
+
+                    int i = command.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dts);
+
+                    string dato = dts.Rows[0]["Id"].ToString();
+
+                    return Json(i, JsonRequestBehavior.AllowGet);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        public JsonResult ActualizarPersona(int Id, string Nombre, string ApellidoPaterno, string ApellidoMaterno, Boolean Estatus)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConexionExamen"].ConnectionString))
+            {
+                DataTable dts = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spU_ActualizaPersona", connection);
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@ApellidoPaterno", ApellidoPaterno);
+                    command.Parameters.AddWithValue("@ApellidoMaterno", ApellidoMaterno);
+                    command.Parameters.AddWithValue("@Estatus", Estatus);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    int iResultado = command.ExecuteNonQuery();
+
+                    return Json(new { Resultado = iResultado }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Json(ex.Message.ToString(), JsonRequestBehavior.AllowGet);
+                }
+            }
         }
 
     }

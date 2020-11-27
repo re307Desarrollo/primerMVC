d9c74e382f97993fee8e9d7497d59e93d48160ac
OswinTelevisa <74924274+OswinTelevisa@users.noreply.github.com>
2020-11-27 16:13:52 -0600

    Delete y Update finalizados

--- miPrimerMVCC/Controllers/HomeController.cs
+++ miPrimerMVCC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -42,16 +43,35 @@ namespace miPrimerMVCC.Controllers
             return Json(new { Message = message, JsonRequestBehavior.AllowGet });
         }
         public JsonResult getPersons()
-        {  
+        {
             return Json(db.personas.ToList(), JsonRequestBehavior.AllowGet);
         }
+
         [HttpPost]
         public ActionResult deletePerson(int id) {
             personas per = db.personas.Find(id);
             db.personas.Remove(per);
+            db.SaveChanges();
             string message = "Deleted Succsfully";
             return Json(new { Message = message, JsonRequestBehavior.AllowGet });
         }
-
+        public ActionResult getID(int id) 
+        {
+            personas pers = db.personas.Find(id);
+            return Json(pers, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult updatePerson(int id, personas pers) 
+        {
+            personas prsonUpdate = db.personas.Find(id);
+            if (!string.IsNullOrWhiteSpace(pers.nombres)) { prsonUpdate.nombres = pers.nombres; }
+            if (!string.IsNullOrWhiteSpace(pers.apellidop)) { prsonUpdate.apellidop = pers.apellidop; }
+            if (!string.IsNullOrWhiteSpace(pers.apellidom)) { prsonUpdate.apellidom = pers.apellidom; }
+            if (!string.IsNullOrWhiteSpace(pers.edad.ToString())) { prsonUpdate.edad = pers.edad; }
+            if (!string.IsNullOrWhiteSpace(pers.sexo)) { prsonUpdate.sexo = pers.sexo; }
+            db.SaveChanges();
+            string mess = "Updated";
+            return Json(new { Message = mess, JsonRequestBehavior.AllowGet });
+        }
     }
 }
